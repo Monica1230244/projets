0ea33097ffac632a43db22c9242f96c11b69eb85
Miriam-anb <anibabamiriam@gmail.com>
2025-04-08 17:15:59 +0200

    add package and supabase insatallation

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:projets/connect_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+
+
+
+  Future<void> main() async {
+    await Supabase.initialize(
+      url: 'https://tryfckvixbjkpjhhyewi.supabase.co',
+      anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRyeWZja3ZpeGJqa3BqaGh5ZXdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQwMTkyMDksImV4cCI6MjA1OTU5NTIwOX0.bUAUjXqg_O3MUTL0pV4GSDz6BNpk6A5EIfMWb72F2II',
+    );
+
+    runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
--- lib/retard_page.dart
+++ lib/retard_page.dart
@@ -48,9 +48,8 @@ class _RetardPageState extends State<RetardPage> {
         leading: IconButton(
           icon: Icon(Icons.arrow_back, color: Colors.black),
           onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => Presence() ),
+            Navigator.pop(
+              context
             );
           },
         ),
